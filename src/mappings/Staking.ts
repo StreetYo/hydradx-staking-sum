68d69814400207d4cfb6fe3a830e66b00709aae0
Nariman <nngasanov@gmail.com>
2021-11-08 12:40:10 +0500

    Use new algorithm to recognize validator rewards

--- src/mappings/Staking.ts
+++ src/mappings/Staking.ts
@@ -3,10 +3,183 @@ import {SubstrateBlock, SubstrateEvent, SubstrateExtrinsic} from "@subql/types";
 import {Balance} from '@polkadot/types/interfaces';
 const md5 = require('md5');
 
+async function getNominatedStake(accountId, validator, era) {
+    const id = md5(accountId + validator + era);
+    return await NominatedStake.get(id);
+}
+
+async function loadNominatedStakes(validator: string, era) {
+    era = parseInt(era);
+
+    let nominators = await api.query.staking.erasStakers(era, validator);
+    let nomins = [];
+
+    // @ts-ignore
+    for(let nominator of nominators.others) {
+        await createNominatedStake(
+            era,
+            nominator.who.toString(),
+            validator.toString(),
+            nominator.value.toBigInt()
+        );
+
+        nomins.push({
+            who: nominator.who.toString(),
+            value: nominator.value.toBigInt()
+        });
+    }
+
+    await createNominatedStake(
+        era,
+        validator.toString(),
+        validator.toString(),
+        //@ts-ignore
+        nominators.own.toBigInt()
+    );
+
+    // nomins.sort(function(a, b) {
+    //     if (a.value > b.value)
+    //         return -1;
+    //
+    //     if (a.value < b.value)
+    //         return 1;
+    //
+    //     return 0;
+    // });
+
+    return nomins;
+}
+
+async function createNominatedStake(era, accountId, validator, nominated) {
+    let id = md5(accountId + validator + era);
+    let nominatedStake = await NominatedStake.get(id);
+
+    if(nominatedStake === undefined) {
+        nominatedStake = new NominatedStake(id);
+        nominatedStake.account = accountId;
+        nominatedStake.validator = validator;
+        nominatedStake.era = era;
+        nominatedStake.nominated = nominated;
+        await nominatedStake.save();
+    }
+}
+
+export async function handlePayoutStakersBatch(extrinsic: SubstrateExtrinsic): Promise<void> {
+    let validators = [];
+    let currentEvent = 0;
+    let validatorsNominators = [];
+    let calls = [];
+
+    if(extrinsic.extrinsic.method.method.toString().toLowerCase() === 'batch') {
+        let [extrCalls] = extrinsic.extrinsic.args;
+        // @ts-ignore
+        calls = extrCalls;
+    } else {
+        calls.push(extrinsic.extrinsic);
+    }
+
+    let rewards = getRewardsFromExtrinsic(extrinsic);
+
+    if(rewards.length === 0) {
+        return;
+    }
+
+    for (let call of calls) {
+        if(call.method.toString() === 'payoutStakers') {
+            let [validator, callEra] = call.args;
+
+            validatorsNominators.push(
+                await loadNominatedStakes(validator.toString(), callEra.toNumber())
+            );
+
+            validators.push({
+                validator: validator.toString(),
+                era: callEra.toNumber(),
+                rewards: []
+            });
+        }
+    }
+
+    if(validators.length === 0) {
+        return;
+    }
+
+    for(let validatorI in validators) {
+        if(rewards[currentEvent].account === validators[validatorI].validator) {
+            validators[validatorI].rewards.push(rewards[currentEvent]);
+            currentEvent++;
+        }
+
+        for(let nominator of validatorsNominators[validatorI]) {
+            if(
+                currentEvent === rewards.length ||
+                rewards[currentEvent].account !== nominator.who
+            ) break;
+
+            validators[validatorI].rewards.push(rewards[currentEvent]);
+            currentEvent++;
+        }
+    }
+
+    await saveValidatorsWithRewards(validators);
+}
+
+function getRewardsFromExtrinsic(extrinsic: SubstrateExtrinsic) {
+    let rewards = [];
+
+    for(let [index, event] of extrinsic.events.entries()) {
+        if (event.event.method.toString().toLowerCase() === 'reward') {
+            const {event: {data: [account, newReward]}} = event;
+            rewards.push({
+                account: account.toString(),
+                reward: (newReward as Balance).toBigInt(),
+                id: `${extrinsic.block.block.header.number.toString()}-${index.toString()}`,
+                date: extrinsic.block.timestamp
+            })
+        }
+    }
+
+    return rewards;
+}
+
+export async function handlePayoutStakers(extrinsic: SubstrateExtrinsic): Promise<void> {
+    let rewards = getRewardsFromExtrinsic(extrinsic);
+
+    if(rewards.length === 0) {
+        return;
+    }
+
+    let [validator, era] = extrinsic.extrinsic.args;
+
+    await saveValidatorsWithRewards([{
+        validator: validator.toString(),
+        // @ts-ignore
+        era: era.toNumber(),
+        rewards: rewards
+    }]);
+}
+
+async function saveValidatorsWithRewards(validatorsWithRewards) {
+    for(let validator of validatorsWithRewards) {
+        for(let reward of validator.rewards) {
+            const entity = new StakingReward(reward.id);
+
+            entity.accountId = reward.account;
+            entity.balance = reward.reward;
+            entity.date = reward.date;
+            entity.era = validator.era;
+            entity.validator = validator.validator;
+
+            await entity.save();
+        }
+    }
+}
+
 async function getValidatorFromRewardEvent(event: SubstrateEvent) {
     if(event.extrinsic.extrinsic.method.method.toString() === 'payoutStakers') {
         const [validator, era] = event.extrinsic.extrinsic.args;
-        return [validator.toString(), false];
+        //@ts-ignore
+        return [era.toNumber(), validator.toString(), false];
     }
 
     const {event: {data: [account, newReward]}} = event;
@@ -19,20 +192,25 @@ async function getValidatorFromRewardEvent(event: SubstrateEvent) {
     calls.map(function(call) {
         if(call.method.toString() === 'payoutStakers') {
             let [validator, callEra] = call.args;
-            validators.push(validator.toString());
+            validators.push({
+                validator: validator.toString(),
+                era: callEra.toNumber()
+            });
         }
     });
 
     if(validators.length === 0) {
-        return [null, false];
+        return [null, null, false];
     } else if(validators.length === 1) {
-        return [validators[0], false];
+        return [validators[0].era, validators[0].validator, false];
     }
 
-    let search = validators.indexOf(account.toString());
+    let search = validators.findIndex(function (validator) {
+        return validator === account.toString();
+    });
 
-    if(search !== -1) {
-        return [account.toString(), false];
+    if(search !== undefined) {
+        return [validators[search].era, account.toString(), false];
     }
 
     let stakes = [];
@@ -45,6 +223,11 @@ async function getValidatorFromRewardEvent(event: SubstrateEvent) {
             stakeValidators.push(nominatedStake.validator);
         }
     }
+
+    logger.info(stakeValidators);
+
+    throw new Error('just');
+
 
     if(stakes.length === 0) {
         return [validators.join(':'), true];
@@ -89,61 +272,6 @@ async function getValidatorFromRewardEvent(event: SubstrateEvent) {
     });
 
     return [stakeValidators[rewardIndex], false];
-}
-
-async function getNominatedStake(accountId, validator, era) {
-    const id = md5(accountId + validator + era);
-    return await NominatedStake.get(id);
-}
-
-async function loadNominatedStakes(block: SubstrateBlock, validator: string, era) {
-    era = parseInt(era);
-
-    // let nominators = await api.query.staking.erasStakers.at(block.block.hash.toString(), era, validator);
-    let nominators = await api.query.staking.erasStakers(era, validator);
-
-    // @ts-ignore
-    for(let nominator of nominators.others) {
-        let accountId = nominator.who;
-        let id = md5(accountId.toString() + validator + era);
-        let nominatedStake = await NominatedStake.get(id);
-
-        if(nominatedStake === undefined) {
-            nominatedStake = new NominatedStake(id);
-            nominatedStake.account = accountId.toString();
-            nominatedStake.validator = validator.toString();
-            nominatedStake.era = era;
-            nominatedStake.nominated = nominator.value.toBigInt();
-            await nominatedStake.save();
-        }
-    }
-}
-
-export async function handlePayoutStakers(extrinsic: SubstrateExtrinsic): Promise<void> {
-    let [validator, era] = extrinsic.extrinsic.args;
-    era = await getBlockEra(extrinsic.block);
-    await loadNominatedStakes(extrinsic.block, validator.toString(), era);
-}
-
-export async function handleStakingRewarded(event: SubstrateEvent): Promise<void> {
-    await handleStakingReward(event)
-}
-
-export async function handleStakingReward(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [account, newReward]}} = event;
-    const entity = new StakingReward(`${event.block.block.header.number}-${event.idx.toString()}`);
-
-    entity.accountId = account.toString();
-    entity.balance = (newReward as Balance).toBigInt();
-    entity.date = event.block.timestamp;
-    entity.era = await getBlockEra(event.block);
-
-    const [validator, multiple] = await getValidatorFromRewardEvent(event);
-
-    entity.validator = validator;
-    entity.multiple = multiple;
-
-    await entity.save();
 }
 
 export async function handleStakingSlashed(event: SubstrateEvent): Promise<void> {
